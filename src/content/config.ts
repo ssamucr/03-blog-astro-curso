5cdd75c0803284130fd238fb92ef94b34e8c9676
ssamucr <ssamucr@gmail.com>
2026-03-24 20:31:02 -0500

    Added author avatar and name, by creating a relationship between collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, reference, z } from "astro:content";
 import { date } from "astro:schema";
 
 const blogCollection = defineCollection({
@@ -8,11 +8,21 @@ const blogCollection = defineCollection({
         date: date(),
         description: z.string(),
         image: image(),
-        author: z.string(),
+        // author: z.string(),
+        author: reference('author'),
         tags: z.array(z.string()),
     })
 })
 
+const authorCollection = defineCollection({
+    type: 'data',
+    schema: ({ image }) => z.object({
+        name: z.string(),
+        avatar: image(),
+    })
+})
+
 export const collections = {
-    blog: blogCollection
+    blog: blogCollection,
+    author: authorCollection,
 }
